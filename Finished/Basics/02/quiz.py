55bc71e265701a7706c03304481cb60d7bfbd12e
Yannick Djomo <67871275+franckya@users.noreply.github.com>
2022-10-20 16:23:00 -0400

    Update quiz.py

--- Finished/Basics/02/quiz.py
+++ Finished/Basics/02/quiz.py
@@ -1,7 +1,3 @@
-# Example file for LinkedIn Learning Course "Python: Build a Quiz App" by Joe Marini
-# The Quiz and Question classes define a particular quiz
-
-
 class Quiz:
     def __init__(self):
         self.name = ""
